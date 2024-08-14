b03c40ffdfc730e79dca9252953688ea8b5a1800
Gauri Suthar <gaurisuthar3696@gmail.com>
2024-08-14 10:12:49 +0530

    add files

---
+++ DAY 8/app.js
@@ -0,0 +1,8 @@
+console.log("Hello Techno");
+
+// alert("Hello techno");
+
+//var,let,const
+
+var name=null;
+console.log(typeof(name));
---
+++ DAY 9/app.js
@@ -0,0 +1,42 @@
+//conditional statements
+//if else,switch,ternary operators
+
+//functions
+// array objects
+//loops
+
+// let count=30;
+
+// if(count==20){
+//     console.log("The count is 20");
+// } else{
+//     console.log("The count is not 20");
+// }
+
+// if(count==20){
+//     console.log("The count is 20");
+// } else if(count>20){
+//     console.log("The count is greater than 20");
+// }else{
+//     console.log("The count is not 20");
+// }
+
+// let firstNum=prompt("Enter the first number");
+// console.log(firstNum);
+
+
+//task
+
+/*let userName=prompt("Enter username");
+let password=+prompt("Enter password");
+
+
+if(userName==="admin" && password===123){
+    console.log("Auth successful");
+} else{
+    console.log("Auth failed");
+}*/
+
+//ternary operator
+let count=30;
+count===20 ? console.log("The count is 20") :count>20 ? console.log("The count is greater than 20"):console.log("The count is not 20");
