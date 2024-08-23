12f2a980ac19de10c0a1ed8a8533ef112aad1273
Gauri Suthar <gaurisuthar3696@gmail.com>
2024-08-23 09:10:53 +0530

    new files

---
+++ TASK 5/script.js
@@ -0,0 +1,13 @@
+// const menuButton = document.getElementById('menu');
+// const sidebar = document.getElementById('sidebar');
+// const closeSidebarButton = document.getElementById('close-sidebar');
+
+// // Open sidebar on menu button click
+// menuButton.addEventListener('click', function() {
+//     sidebar.classList.add('active');
+// });
+
+// // Close sidebar on close button click
+// closeSidebarButton.addEventListener('click', function() {
+//     sidebar.classList.remove('active');
+// });
