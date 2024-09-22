755e2d0a39826f5fab8cbccac15098fdc9bf6ee4
Gauri Suthar <gaurisuthar3696@gmail.com>
2024-09-22 16:16:43 +0530

    add files

---
+++ REACT/math.js
@@ -0,0 +1,20 @@
+/*export const add=(a,b)=>{   //named export
+    return a+b;
+}
+
+export const mul=(a,b)=>{
+    return a*b;
+}
+
+const divide=(a,b)=>{
+    return a/b;0
+}
+
+export default divide; */ //default export
+//to import default we dont have to use curly braces,and for export default we have to define in
+//  another line because sometimes js can occur porblem
+
+//camel case->firstName,varible,functions
+//Pascal Case-> component name,
+//snack case-> security 
+
---
+++ REACT/script.js
@@ -0,0 +1,9 @@
+// import divide,{add as sum,mul} from "./math.js";
+
+// import *as math from "./math.js"; //default export does not come in this
+// import divide from "./math.js"
+
+// console.log(math.add(10,20));
+// console.log(math.mul(10,20));
+// console.log(divide(20,10));
+
